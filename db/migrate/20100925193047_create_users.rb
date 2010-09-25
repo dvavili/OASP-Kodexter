58c7ad3a8f3e5d3aca26f5e78efe1b0245c5adac
Divya Vavili <divyavavili@cmu.edu>
2010-09-25 15:37:30 -0400

    First commit for OASP

---
+++ db/migrate/20100925193047_create_users.rb
@@ -0,0 +1,22 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :name
+      t.string :address
+      t.string :city
+      t.string :country
+      t.integer :zipcode
+      t.integer :phone
+      t.string :email
+      t.string :salt
+      t.string :username
+      t.string :password
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
